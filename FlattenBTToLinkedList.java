59840e886ae70143e5b83d52fb3281945f746370
Steven Dong <steven7dong@gmail.com>
2019-02-12 10:39:22 -0500

    WordLadder MergeKSortedArray TopologicalSort DivideAndConquer

---
+++ FlattenBTToLinkedList.java
@@ -0,0 +1,42 @@
+public class FlattenBTToLinkedList {
+
+    public class TreeNode {
+        public int val;
+        public TreeNode left, right;
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+
+    public void flatten(TreeNode root) {
+        helper(root);
+    }
+
+    // flatten root and return the last node
+    private TreeNode helper(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode leftLast = helper(root.left);
+        TreeNode rightLast = helper(root.right);
+
+        // connect leftLast to root.right
+        if (leftLast != null) {
+            leftLast.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+
+        if (rightLast != null) {
+            return rightLast;
+        }
+
+        if (leftLast != null) {
+            return leftLast;
+        }
+
+        return root;
+    }
+}
